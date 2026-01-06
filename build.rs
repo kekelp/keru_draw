26e9373febc5e50a6d2ee0eaa42632057e5281b8
kekelp <kekelp@kekelp>
2026-01-06 10:55:08 +0100

    rename shaders

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 fn main() {
-    println!("cargo:rerun-if-changed=src/shaders/text.slang");
+    println!("cargo:rerun-if-changed=src/shaders/textslabs.slang");
     println!("cargo:rerun-if-changed=src/shaders/keru_images.slang");
 
-    let imported_textslabs = include_str!("src/shaders/text.slang");
-    let original_textslabs = textslabs::TextRenderer::composable_shader_source();
-    assert!(imported_textslabs == original_textslabs, "Imported textslabs shader does not match original!");
+    let imported_textslabs_shader = include_str!("src/shaders/textslabs.slang");
+    let original_textslabs_shader = textslabs::TextRenderer::composable_shader_source();
+    assert!(imported_textslabs_shader == original_textslabs_shader, "Imported textslabs shader does not match original!");
 
-    let imported_images = include_str!("src/shaders/keru_images.slang");
-    let original_images = keru_images::ImageRenderer::composable_shader_source();
-    assert!(imported_images == original_images, "Imported keru_images shader does not match original!");
+    let imported_images_shader = include_str!("src/shaders/keru_images.slang");
+    let original_images_shader = keru_images::ImageRenderer::composable_shader_source();
+    assert!(imported_images_shader == original_images_shader, "Imported keru_images shader does not match original!");
 }
